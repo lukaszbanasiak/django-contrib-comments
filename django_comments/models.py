88ee32f68484a656f4e63e76794099e3a26fc898
Łukasz Banasiak <lukasz@banasiak.me>
2014-12-20 14:16:02 +0100

    separate models for non-auth user, auth user and for old approach

--- django_comments/models.py
+++ django_comments/models.py
@@ -8,6 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
+import django_comments
 from django_comments.managers import CommentManager
 
 COMMENT_MAX_LENGTH = getattr(settings, 'COMMENT_MAX_LENGTH', 3000)
@@ -43,20 +44,10 @@ class BaseCommentAbstractModel(models.Model):
 
 
 @python_2_unicode_compatible
-class Comment(BaseCommentAbstractModel):
+class BaseCommentModel(BaseCommentAbstractModel):
     """
-    A user comment about some object.
+    A base comment model.
     """
-
-    # Who posted this comment? If ``user`` is set then it was an authenticated
-    # user; otherwise at least user_name should have been set and the comment
-    # was posted by a non-authenticated user.
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('user'),
-                    blank=True, null=True, related_name="%(class)s_comments")
-    user_name = models.CharField(_("user's name"), max_length=50, blank=True)
-    user_email = models.EmailField(_("user's email address"), blank=True)
-    user_url = models.URLField(_("user's URL"), blank=True)
-
     comment = models.TextField(_('comment'), max_length=COMMENT_MAX_LENGTH)
 
     # Metadata about the comment
@@ -74,6 +65,7 @@ class Comment(BaseCommentAbstractModel):
     objects = CommentManager()
 
     class Meta:
+        abstract = True
         db_table = "django_comments"
         ordering = ('submit_date',)
         permissions = [("can_moderate", "Can moderate comments")]
@@ -86,7 +78,38 @@ class Comment(BaseCommentAbstractModel):
     def save(self, *args, **kwargs):
         if self.submit_date is None:
             self.submit_date = timezone.now()
-        super(Comment, self).save(*args, **kwargs)
+        super(BaseCommentModel, self).save(*args, **kwargs)
+
+    def get_absolute_url(self, anchor_pattern="#c%(id)s"):
+        return self.get_content_object_url() + (anchor_pattern % self.__dict__)
+
+    def get_as_text(self):
+        """
+        Return this comment as plain text.  Useful for emails.
+        """
+        d = {
+            'user': self.user or self.name,
+            'date': self.submit_date,
+            'comment': self.comment,
+            'domain': self.site.domain,
+            'url': self.get_absolute_url()
+        }
+        return _('Posted by %(user)s at %(date)s\n\n%(comment)s\n\nhttp://%(domain)s%(url)s') % d
+
+
+class LegacyComment(BaseCommentModel):
+    """
+    A legacy comment model.
+    """
+
+    # Who posted this comment? If ``user`` is set then it was an authenticated
+    # user; otherwise at least user_name should have been set and the comment
+    # was posted by a non-authenticated user.
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('user'),
+                    blank=True, null=True, related_name="%(class)s_comments")
+    user_name = models.CharField(_("user's name"), max_length=50, blank=True)
+    user_email = models.EmailField(_("user's email address"), blank=True)
+    user_url = models.URLField(_("user's URL"), blank=True)
 
     def _get_userinfo(self):
         """
@@ -144,21 +167,90 @@ class Comment(BaseCommentAbstractModel):
         self.user_url = val
     url = property(_get_url, _set_url, doc="The URL given by the user who posted this comment")
 
-    def get_absolute_url(self, anchor_pattern="#c%(id)s"):
-        return self.get_content_object_url() + (anchor_pattern % self.__dict__)
+    class Meta(BaseCommentModel.Meta):
+        managed = False
 
-    def get_as_text(self):
+
+class NonAuthComment(BaseCommentModel):
+    """
+    A non-authenticated user comment model.
+    """
+    user_name = models.CharField(_("user's name"), max_length=50, blank=True)
+    user_email = models.EmailField(_("user's email address"), blank=True)
+    user_url = models.URLField(_("user's URL"), blank=True)
+
+    def _get_userinfo(self):
         """
-        Return this comment as plain text.  Useful for emails.
+        Get a dictionary that pulls together information about the poster
+        safely for non-authenticated comments.
+
+        This dict will have ``name``, ``email``, and ``url`` fields.
         """
-        d = {
-            'user': self.user or self.name,
-            'date': self.submit_date,
-            'comment': self.comment,
-            'domain': self.site.domain,
-            'url': self.get_absolute_url()
-        }
-        return _('Posted by %(user)s at %(date)s\n\n%(comment)s\n\nhttp://%(domain)s%(url)s') % d
+        if not hasattr(self, "_userinfo"):
+            userinfo = {
+                "name": self.user_name,
+                "email": self.user_email,
+                "url": self.user_url
+            }
+            self._userinfo = userinfo
+        return self._userinfo
+    userinfo = property(_get_userinfo, doc=_get_userinfo.__doc__)
+
+    def _get_name(self):
+        return self.userinfo["name"]
+
+    def _set_name(self, val):
+        self.user_name = val
+    name = property(_get_name, _set_name, doc="The name of the user who posted this comment")
+
+    def _get_email(self):
+        return self.userinfo["email"]
+
+    def _set_email(self, val):
+        self.user_email = val
+    email = property(_get_email, _set_email, doc="The email of the user who posted this comment")
+
+    def _get_url(self):
+        return self.userinfo["url"]
+
+    def _set_url(self, val):
+        self.user_url = val
+    url = property(_get_url, _set_url, doc="The URL given by the user who posted this comment")
+
+    class Meta(BaseCommentModel.Meta):
+        managed = False
+
+
+class AuthComment(BaseCommentModel):
+    """
+    A authenticated user comment model.
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('user'),
+                    blank=True, null=True, related_name="%(class)s_comments")
+
+    def _get_name(self):
+        if self.user.get_full_name():
+            return self.user.get_full_name()
+        return self.user.get_username()
+
+    def _set_name(self, val):
+        raise AttributeError(_("This comment was posted by an authenticated "\
+                                   "user and thus the name is read-only."))
+    name = property(_get_name, _set_name, doc="The name of the user who posted this comment")
+
+    def _get_email(self):
+        return self.user.email
+
+    def _set_email(self, val):
+        raise AttributeError(_("This comment was posted by an authenticated "\
+                                   "user and thus the email is read-only."))
+    email = property(_get_email, _set_email, doc="The email of the user who posted this comment")
+
+    class Meta(BaseCommentModel.Meta):
+        managed = False
+
+# Comment model for backward capability
+Comment = django_comments.get_model()
 
 
 @python_2_unicode_compatible
